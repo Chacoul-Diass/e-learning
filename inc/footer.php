9d452b6e2ae27a8085c0e128bb3a41a60e405238
Aklamzeeerrrr <54205051+Aklamzeeerrrr@users.noreply.github.com>
2020-05-11 00:08:12 +0000

    Partie utilisateur

---
+++ inc/footer.php
@@ -0,0 +1,58 @@
+<div id="footer">
+    <ul>
+        <li>
+            <h2><span>Apr</span>opos De Nous</h2>
+            <p>Nous Sommes Un Jeune Groupe D'étudiants Qualifiés Qui Concevons Des Sites Web Dynamiques Rien Que Vous Avec Aussi Des Bonbons!</p>
+        </li>
+        <li>
+            <h2><span>Con</span>tactez-Nous</h2>
+            <table>
+                <tr>
+                    <td><i class="fas fa-map-marker-alt"></i></td>
+                    <td>Cote dIvoire, Abidjan, Yopougon</td>
+                </tr>
+                <tr>
+                    <td><i class="fas fa-phone-square"></i></td>
+                    <td>+225 03892083</td>
+                </tr>
+                <tr>
+                    <td><i class="fas fa-envelope"></i></td>
+                    <td>akragba29@gmail.com</td>
+                </tr>
+            </table>
+            <div id="f_partager">
+                <div id="fb">
+                    <a href="#">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                </div>
+                <div id="gp">
+                    <a href="#">
+                        <i class="fab fa-google-plus-g"></i>
+                    </a>
+                </div>
+                <div id="tw">
+                    <a href="#">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </div>
+            </div>
+        </li>
+        <li>
+            <h2><span>Lai</span>ssez Nous Un Message</h2>
+            <form method="post">
+                <div id="f_input">
+                    <i class="fas fa-user"></i>
+                    <input type="text" name="requete_nom" placeholder="Entrer Votre Nom">
+                </div>
+                <div id="f_input">
+                    <i class="fas fa-envelope"></i>
+                    <input type="text" name="requete_email" placeholder="Entrer Votre Nom">
+                </div>
+                <textarea name="msg" placeholder="Entrer Votre message"></textarea>
+                <button>Envoyer</button>
+            </form>
+        </li><br clear="all">
+    </ul>
+    <h3>All Rights Reserve To Armel Kragba Copyright © 2020 Armel Kragba.</h3>
+</div>
---
+++ inc/header.php
@@ -0,0 +1,104 @@
+<div id="header">
+    <div id="up_head">
+        <div id="lien">
+            <ul>
+                <li>
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-google-plus"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-linkedin"></i></a>
+                </li>
+            </ul>
+        </div>
+        <div id="date">
+            <p>
+                <?php
+                    echo date('l, d F Y');
+                ?>
+            </p>
+        </div>
+        <div id="slog">
+            <p>
+                Numero 1 des Sites de E learning Ivoirien
+            </p>
+        </div>
+    </div>
+    <div id="titre">
+        <h2><a href="index.php">E Learning</a></h2>
+    </div>
+    <div id="menu">
+        <h2>
+            <i class="fas fa-bars"></i>
+            <?php
+                include("inc/cat_menu.php");
+            ?>
+        </h2>
+    </div>
+    <div id="head_recherche">
+        <form method="post" enctype="multipart/form-data">
+            <input type="recherche" name="requete" placeholder="Rechercher Cours Ici">
+            <button name="recherche"><i class="fas fa-search"></i></button>
+        </form>
+    </div>
+    <div id="head_panier">
+        <a href="panier.php"><i class="fas fa-shopping-cart"></i> <span>0</span></a>
+    </div>
+    <div id="head_connexion">
+        <h4><i class="fas fa-user"></i> Connexion</h4>
+        <form method="post">
+            <center>
+                <h3><i class="fas fa-user"></i></h3>
+                <h4>Connexion</h4>
+            </center>
+            <div id="input_f">
+                <i class="fas fa-envelope"></i>
+                <input type="text" name="u_email" placeholder="Entrer Votre Adresse Mail">
+            </div>
+            <div id="input_f">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="u_pass" placeholder="Entrer Votre Mot De Passe">
+            </div>
+            <h5>Mot De Passe Oublié ?</h5><br clear="all">
+            <center><button name="connexion">Connexion</button></center>
+        </form>
+    </div>
+    <div id="head_sinscrire">
+        <h4><i class="fas fa-user-plus"></i> S'inscrire</h4>
+        <form method="post">
+            <center>
+                <h3><i class="fas fa-user-plus"></i></h3>
+                <h4>S'inscrire</h4>
+            </center>
+            <div id="input_f">
+                <i class="fas fa-user"></i>
+                <input type="text" name="s_nom" placeholder="Entrer Votre Nom">
+            </div>
+            <div id="input_f">
+                <i class="fas fa-envelope"></i>
+                <input type="text" name="s_email" placeholder="Entrer Votre Adresse Mail">
+            </div>
+            <div id="input_f">
+                <i class="fas fa-phone"></i>
+                <input type="text" name="s_tel" placeholder="Entrer Votre N° De Téléphone">
+            </div>
+            <div id="input_f">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="s_pass1" placeholder="Entrer Votre Mot De Passe">
+            </div>
+            <div id="input_f">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="s_pass2" placeholder="Réentrer Votre Mot De Passe">
+            </div>
+            <center><button name="s_sinscrire">S'inscrire</button></center>
+        </form>
+    </div>
+</div>
